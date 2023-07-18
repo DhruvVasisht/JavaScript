1d43e29a58c9cb676f378e9339e0edd031048fcf
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-18 14:07:15 +0530

    Day 58

--- Data Types/main.js
+++ Data Types/main.js
@@ -1,10 +1,22 @@
 let a=23;
+a=a+1;
 console.log("The Value Of a Is:",a);
 console.log("the Type of a is:",typeof(a));
 
-a="Dhruv"
+a='23'
+a=a+1;
+console.log("The Value Of a Is:",a);
+
+a="My Name Is Dhruv"
 console.log(a)
 console.log(typeof(a));
 
 b=false
 console.log(typeof(b));
+
+let c=null;
+console.log("c is:",c);
+
+let d=undefined;
+d=Number(d);
+console.log(d);
