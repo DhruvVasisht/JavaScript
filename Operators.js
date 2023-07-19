0aea7bc84722972f45dd64bfb585f0d5c616a4b7
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-19 13:37:41 +0530

    Day 59

---
+++ Operators.js
@@ -0,0 +1,25 @@
+const FirstNumber=3;
+//const SecondNumber=4;
+const SecondNumber="3";
+console.log(FirstNumber+SecondNumber);
+console.log(FirstNumber%SecondNumber);
+
+const result=(FirstNumber==SecondNumber);
+console.log(result);
+const Triple=(FirstNumber===SecondNumber);
+console.log(Triple);
+
+let a=24;
+console.log(a);
+a++;
+console.log(a);
+
+let ThirdNumber=12;
+ThirdNumber+=4;
+console.log(ThirdNumber);
+
+let d=true;
+let e=false;
+console.log(d&&e);
+console.log(d||e);
+console.log(!d);
