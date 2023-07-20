d4dedb8ee8c60ca332e06b28849713bc8771df35
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-20 13:40:01 +0530

    Day 60

---
+++ Switch Case/main.js
@@ -0,0 +1,20 @@
+const grade="G"
+switch(grade){
+    case "A":
+        console.log("A -> Very Good");
+        break;
+    case "B":
+        console.log("B -> Good");
+        break;
+    case "C":
+        console.log("C -> Do Better Next Time");
+        break;
+    case "D":
+    case "E":
+        console.log("Improve Next Time");
+        break;
+    
+    default:
+        console.log("Invalid Grade");
+
+}
