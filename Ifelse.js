26e71ac7eebe28dfe8509b64f8118cc93b18043a
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-20 13:14:23 +0530

    Day 60

---
+++ Ifelse.js
@@ -0,0 +1,37 @@
+//const age=660;
+//
+//if(age>=18){
+//    console.log("You Are Inside If ");
+//    console.log("You can Vote");
+//}
+//
+// if(age>60){
+//    console.log("You Can Vote Again");
+// }
+//
+//else{
+//    console.log("You Cant Vote");
+//}
+
+//console.log("This Line Will Execute Always");
+
+//const marks=89;
+//if(marks>90){
+//    console.log("A+");
+//}
+//else if(marks>80){
+//    console.log("B+");
+//}
+//else if(marks>70){
+//    console.log("C+");
+//}
+//else if(marks>60){
+//    console.log("D+");
+//}
+//else{
+//    console.log("Low Marks");
+//}
+
+const marks=60;
+const result=(marks>=50)? "Passed" : "Failed";
+console.log(result);
