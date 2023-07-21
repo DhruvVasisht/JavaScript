ea81a1ac93727a77ea302c6ac9886c982a8c8d66
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-21 12:22:07 +0530

    Day 61

---
+++ Loops/main.js
@@ -0,0 +1,13 @@
+//for(let index=0;index<=6;index=index+2){
+//   console.log('Dhruv',index);
+//}
+
+let step=0;
+while(step<5){
+    console.log('step',step);
+    step=step+1
+    if(step==2){
+        //break;
+        continue;
+    }
+}
