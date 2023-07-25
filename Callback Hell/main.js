8d12b99daafe4dcfef1e25a465b906be6e24eff4
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-25 12:09:07 +0530

    Day 64

--- Callback Hell/main.js
+++ Callback Hell/main.js
@@ -1,6 +1,42 @@
-console.log("Hell0");
-setTimeout(function(){
-    console.log("This Will Execute Later");
-},3000);
-console.log("Wprld");
-console.log("Three");
+//console.log("Hell0");
+//setTimeout(function(){
+//    console.log("This Will Execute Later");
+//},3000);
+//console.log("Wprld");
+//console.log("Three");
+
+function getCheese(callback){
+    setTimeout(() => {
+        const cheese= '🧀';
+        console.log("Here Is Cheese",cheese);
+        callback(cheese);
+    }, 2000);
+    
+}
+
+function makeDough(cheese,callback){
+    setTimeout(() => {
+       const dough=cheese+ "🎂" ;
+       console.log("Here Is the Dough",dough);
+       callback(dough)
+    }, 2000);
+}
+
+function bakePizza(dough,callback){
+    setTimeout(() => {
+       const pizza=dough+ "🍕" ;
+       console.log("Here Is the Pizza",pizza);
+       callback(pizza)
+    }, 2000);
+}
+
+
+getCheese((cheese)=> {
+    makeDough(cheese,(dough) => {
+        bakePizza(dough,(pizza)=>{
+            console.log("Got Pizza",pizza);
+        });
+        
+    })
+
+});
